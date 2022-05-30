e8635e343c4c3ab0b67fc3ca95371a4416e2e383
Muhammed Bahadır Antlı <bahadir.antli@pru.edu.tr>
2022-05-30 14:51:34 +0300

    Product Tracking System
    Product Tracking System with add, delete and update buttons.

---
+++ StokKayit-W12/StokKayit-W12/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;   //For connection to database
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokKayit_W12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-N3ECN9S; Initial Catalog=Stok3; Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {//ADD
+             
+            String t1 = textBox1.Text;  // Product Code
+            String t2 = textBox2.Text;  // Product Name
+            String t3 = textBox3.Text;  // Annual Sale
+            String t4 = textBox4.Text;  // Unit Price
+            String t5 = textBox5.Text;  // Min Stock
+            String t6 = textBox6.Text;  // Supply Time
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO Malzemeler (MalzemeKodu, MalzemeAdi, YillikSatis, BirimFiyat, MinStok, TSuresi) VALUES('" + t1 + "','" + t2 + "','" + t3 + "','" + t4 + "','" + t5 + "','" + t6 + "' )  ", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void listele()  // Appearance of records in the database
+        {
+            baglanti.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select *from Malzemeler", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;   
+            baglanti.Close();   
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {// DELETE
+            String t1 = textBox1.Text;
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("DELETE FROM Malzemeler WHERE MalzemeKodu=('"+t1+"')", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {// UPDATE
+
+            String t1 = textBox1.Text;  // Product Code
+            String t2 = textBox2.Text;  // Product Name
+            String t3 = textBox3.Text;  // Annual Sale
+            String t4 = textBox4.Text;  // Unit Price
+            String t5 = textBox5.Text;  // Min Stock
+            String t6 = textBox6.Text;  // Supply Time
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("UPDATE Malzemeler SET MalzemeKodu='" + t1 + "', MalzemeAdi='" + t2 + "', YillikSatis='" + t3 + "', BirimFiyat='" + t4 + "', MinStok='" + t5 + "', TSuresi='" + t6 + "' WHERE MalzemeKodu='" + t1 + "' ", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int[] sayilar = new int[30];
+            for (int i = 0; i < 30; i++)
+            {
+                sayilar[i] = rnd.Next(0, 100);
+
+            }
+            Array.Sort(sayilar);
+
+            foreach (int eleman in sayilar)
+            {
+                listBox1.Items.Add(eleman);
+            }
+        }
+    }
+}
---
+++ StokKayit-W12/StokKayit-W12/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace StokKayit_W12
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlDataReader dr;
+        SqlCommand com;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string password = textBox2.Text;
+            con = new SqlConnection("Data Source=DESKTOP-N3ECN9S;Initial Catalog=Stok3;Integrated Security=True");
+            com = new SqlCommand();
+            con.Open(); 
+            com.Connection = con;
+            com.CommandText = "Select*From Kullanici_Bilgi where kullanici_adi = '" + textBox1.Text + "' And sifre = '" + textBox2.Text + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Successfully Entered!");
+                Form1 fr1 = new Form1();
+                fr1.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password!");
+            }
+            con.Close();
+        }
+    }
+}
